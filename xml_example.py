2419b9c74be73376594bfc5a61bbd7021b43a3d5
Bigmafusail <rudov.dmitriy@gmail.com>
2026-05-25 15:57:30 +0400

    Форматы представления данных

---
+++ xml_example.py
@@ -0,0 +1,19 @@
+import xml.etree.ElementTree as ET
+
+xml_data = """
+<user>
+    <id>1</id>
+    <first_name>John</first_name>
+    <last_name>Doe</last_name>
+    <email>john.doe@example.com</email>
+    <address>
+        <street>Main Street 1</street>
+        <city>New York</city>
+        <zip>10001</zip>
+    </address>
+</user>
+"""
+
+root = ET.fromstring(xml_data)
+
+print('User ID:', root.find('id').text)
