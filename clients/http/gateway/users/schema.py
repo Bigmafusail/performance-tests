b590908097e1c23a2137c388fee67b8857dbd48c
Bigmafusail <rudov.dmitriy@gmail.com>
2026-06-03 16:31:06 +0400

    Знакомство с Faker

--- clients/http/gateway/users/schema.py
+++ clients/http/gateway/users/schema.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, Field, EmailStr, ConfigDict
-
+from tools.fakers import fake
 
 class UserSchema(BaseModel):
     """
@@ -26,11 +26,16 @@ class CreateUserRequestSchema(BaseModel):
     """
     model_config = ConfigDict(populate_by_name=True)
 
-    email: EmailStr
-    last_name: str = Field(alias="lastName")  # Использовали alise
-    first_name: str = Field(alias="firstName")  # Использовали alise
-    middle_name: str = Field(alias="middleName")  # Использовали alise
-    phone_number: str = Field(alias="phoneNumber")  # Использовали alise
+    # Добавили генерацию случайного email
+    email: EmailStr = Field(default_factory=fake.email)
+    # Добавили генерацию случайной фамилии
+    last_name: str = Field(alias="lastName", default_factory=fake.last_name)
+    # Добавили генерацию случайного имени
+    first_name: str = Field(alias="firstName", default_factory=fake.first_name)
+    # Добавили генерацию случайного отчества
+    middle_name: str = Field(alias="middleName", default_factory=fake.middle_name)
+    # Добавили генерацию случайного номер телефона
+    phone_number: str = Field(alias="phoneNumber", default_factory=fake.phone_number)
 
 
 class CreateUserResponseSchema(BaseModel):
